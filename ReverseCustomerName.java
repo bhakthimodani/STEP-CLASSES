79ab9a59e18f88249984080b167e5e0bed410868
bhakthimodani <daniithi78@gmail.com>
2026-09-18 22:28:33 +0530

    Add files via upload

---
+++ ReverseCustomerName.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+
+    static String reverseCustomerName(String customerName) {
+        char[] arr = customerName.toCharArray();
+
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter customer name: ");
+        String customerName = sc.nextLine();
+
+        String reversed = reverseCustomerName(customerName);
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reversed);
+    }
+}
